5d3394aba5818bf397d2d7dbf8ac285090406c75
Pravdeep Kullar <pravdeepkk@gmail.com>
2025-01-28 14:41:55 -0500

    saidhasoid

---
+++ Labs/lab01/part1_modules/lab1.js
@@ -0,0 +1,11 @@
+import _ from "lodash";
+
+const holidays = [
+    { name: "New Year", date: "2026-01-01" },
+    { name: "Valentine's Day", date: "2026-02-14" },
+    { name: "Christmas", date: "2025-12-25" },
+    { name: "Canada Day", date: "2025-07-01" },
+    { name: "Halloween", date: "2025-10-31" },
+    { name: "Thanksgiving", date: "2025-11-27" },
+    { name: "Remembrance Day", date: "2025-11-11" },
+  ];
