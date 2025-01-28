5e0f8424af974acf4b26fbbfcba53852f82450ec
Pravdeep Kullar <pravdeepkk@gmail.com>
2025-01-28 11:58:08 -0500

    test

---
+++ lab02/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import lab_router from "./routers/lab_router.js";
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+
+//imported routes
+// -> localhost:8000/lab
+app.use("/lab", lab_router);
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the server");
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
---
+++ lab02/routers/lab_router.js
@@ -0,0 +1,68 @@
+/* 
+    /name
+    /greeting
+    /add
+    /calculate
+*/
+
+import express from "express";
+
+const router = express.Router();
+
+//checking if in route
+router.get("/", (req, res) => {
+  res.send("welcome to the lab router");
+});
+
+//name route
+router.get("/name", (req, res) => {
+  res.send("Pravdeep Kullar");
+});
+
+//greeting
+router.get("/greeting", (req, res) => {
+  res.send("Hello from Pravdeep, Student number: n01430968");
+});
+
+//add
+router.get("/add/:x/:y", (req, res) => {
+  let x = parseFloat(req.params.x);
+  let y = parseFloat(req.params.y);
+  res.send(`${x + y}`);
+});
+
+//calculate
+router.get("/calculate/:a/:b/:operation", (req, res) => {
+  let a = parseFloat(req.params.a);
+  let b = parseFloat(req.params.b);
+  let operation = req.params.operation;
+  let result = 0;
+
+  switch (operation) {
+    case "+":
+      result = a + b;
+      break;
+
+    case "-":
+      result = a - b;
+      break;
+
+    case "*":
+      result = a * b;
+      break;
+
+      case "/": // -> %2f
+        if (b === 0) {
+          return res.send("Error: Division by zero is not allowed");
+        }
+        result = a / b;
+        break;
+
+    default:
+      res.send("Invalid operation");
+      break;
+  }
+  res.send(`Result: ${result}`);
+});
+
+export default router;
