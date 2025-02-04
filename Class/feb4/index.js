00711b02a5233132a6ebfb8f01e50ba2e723af5c
Pravdeep Kullar <pravdeepkk@gmail.com>
2025-02-04 09:09:36 -0500

    created a middleware folder

--- Class/feb4/index.js
+++ Class/feb4/index.js
@@ -6,28 +6,23 @@
     npm i express nodemon
     (optional) -> go into package.json and add "type": "module" to enable import from 
 */
- 
-// [Please enable only ONE of these] 
+
+// [Please enable only ONE of these]
 import express from "express"; // if you are using type: module
+import logger from "./middleware/logger.js";
 // const express = require("express"); // if using common JS (Default)
- 
+
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-const logger = () => {
-  console.log(req.url);
-  console.log(req.method);
-  console.log(req.headers);
-  console.log(Date());
-}
- 
+app.use(logger); //for application wides (so it can run everywhere)
+
 // middlelware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
- 
-// routes
-app.get("/", (req, res) => {
 
+// routes
+app.get("/", logger, (req, res) => { //using it in a route
   res.send("Welcome to our server");
 });
 
@@ -46,11 +41,11 @@ app.get("/login", (req, res) => {
 app.get("/fetchData", (req, res) => {
   res.send("Welcome to our server");
 });
- 
+
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
- 
+
 app.use("", (req, res) => {
   res.status(404).send("Page not found");
 });
