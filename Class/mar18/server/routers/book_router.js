69511b5922d7e8d5e4bcd2953edd3ae2ea208902
PravdeepK <118395938+PravdeepK@users.noreply.github.com>
2025-03-18 09:29:00 -0400

    ytr

--- Class/mar18/server/routers/book_router.js
+++ Class/mar18/server/routers/book_router.js
@@ -15,4 +15,30 @@ router.get("/:id", (req, res) => {
   });
 });
 
+// 3 search
+router.get("/search", (req, res) => {
+  const filters = {};
+  //query
+  if (req.query.title) {
+    filters.title = req.query.title; //you can convert to mongoose stuff to make it more complex
+  }
+  if (req.query.pages) {
+    let pages = parseInt(req.query.pages);
+    if (req.query.logicalOperator) {
+      switch (req.query.logicalOperator) {
+        case gte:
+          filters.pages = { $gte: { pages } };
+
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
+
+  Book.find(filters).then((results) => {
+    res.json(results);
+  });
+});
 export default router;
